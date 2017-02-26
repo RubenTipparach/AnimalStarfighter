bcd815742c38598f4fbb49a220b1cdd86d54d5d4
Rubensaccnt <santipab.stephane@gmail.com>
2017-02-25 22:12:44 -0600

    Initial Commit.

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ShipController : NetworkBehaviour
+{
+
+
+    [SerializeField]
+    Camera myCamera;
+
+    [SerializeField]
+    AudioListener audioListener;
+
+    [SerializeField]
+    MagnetSensor magnetSensor; // there porbablt exists a function for this, just bypassing for now.
+
+    [SerializeField]
+    GvrAudioListener gaList;
+
+    Transform pointer;
+
+    // Use this for initialization
+	void Start ()
+    {
+        if (isLocalPlayer)
+        {
+            myCamera.enabled = true;
+            audioListener.enabled = true;
+            magnetSensor.enabled = true;
+            gaList.enabled = true;
+            GvrViewer.AddStereoControllerToCameras();
+            magnetSensor.OnCardboardTrigger += Move;
+            pointer = transform.GetChild(0);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(isLocalPlayer)
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                Move();
+            }
+        }
+	}
+
+    void Move()
+    {
+        if (isLocalPlayer)
+        {
+            transform.position += pointer.forward * Time.deltaTime;
+        }
+    }
+}
